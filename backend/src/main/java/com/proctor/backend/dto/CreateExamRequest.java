f581790c551e9838220cb3da25d64f80d142bb04
TusharS230 <tusharsinghdec@gmail.com>
2026-06-10 22:17:22 +0530

    feat: iplement 1:N exam and question relational architecture with cascade saving

---
+++ backend/src/main/java/com/proctor/backend/dto/CreateExamRequest.java
@@ -0,0 +1,20 @@
+package com.proctor.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateExamRequest {
+    private String title;
+    private String description;
+    private Integer durationMinutes;
+
+    private List<QuestionDto> questions;
+}
